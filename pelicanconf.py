b63c085dc8466f44e0b5c30ac551373c282b29b9
Ricardo Lafuente <r@manufacturaindependente.org>
2015-03-09 23:02:15 +0100

    Add gallery plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
 
 AUTHOR = u'Manufactura Independente'
 SITENAME = u'Manufactura Independente'
@@ -47,11 +48,35 @@ ARTICLE_SAVE_AS = '{slug}/index.html'
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
 
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_PATH = "output/"
 
-#COVER_IMG_URL = 'theme/images/cutis.jpg'
-#PROFILE_IMAGE_URL =  'theme/images/cutis.jpg'
-#TAGLINE = 'A Manufactura é que é.'
-#SOCIAL = (
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["gallery"]
+
+# Gallery plug-in
+GALLERY_FOLDER = "galleries"
+GALLERY_SRC_PATH = "%s%s" % (BASE_PATH, "gallery_src")
+GALLERY_OUTPUT_PATH = "%s%s%s" % (BASE_PATH, OUTPUT_PATH, GALLERY_FOLDER)
+GALLERY_REGENERATE_EXISTING = False
+GALLERY_PRESETS = [
+    {"name": "thumb", "actions": [{"type": "fit", "height": 100, "width": 100, "from": (0.5, 0.5)}]},
+    {"name": "slider", "actions": [{"type": "fit", "height": 300, "width": 900, "from": (0.5, 0.5)}]},
+    {"name": "large", "actions": [{"type": "resize", "height": 640, "width": 850, "from": (0.5, 0.5)}]},
+    {"name": "thumb_greyscale",
+        "actions": [
+            {"type": "fit", "height": 100, "width": 100, "from": (0.5, 0.5)},
+            {"type": "greyscale"}
+        ]},
+]
+# This setting is optional, used for thumbnails in bootstrap
+# THUMBNAIL_GALLERY_CLASS = "span2"
+
+
+# COVER_IMG_URL = 'theme/images/cutis.jpg'
+# PROFILE_IMAGE_URL =  'theme/images/cutis.jpg'
+# TAGLINE = 'A Manufactura é que é.'
+# SOCIAL = (
 #            ('github', 'https://github.com/manufacturaind/'),
 #            ('twitter-square', 'https://twitter.com/manufacturaind'),
 #         )
